c1ef3cee6ceba1542525cb173ab6b9b84ae32c64
lucasmeloo1 <lucasmelo130808@gmail.com>
2025-09-08 19:15:58 -0300

    Add files via upload

---
+++ SchoolAverage.py
@@ -0,0 +1,7 @@
+# Average of four student scores
+n = int(input('Enter a number:'))
+n1 = int(input('Enter another number:'))
+n2 = int(input('Enter one more number:'))
+n3 = int(input('Enter the last number:'))
+m = (n+n1+n2+n3)/4
+print('The average student score between {}, {}, {} and {} is {:.2f}'.format(n, n1, n2, n3, m))
