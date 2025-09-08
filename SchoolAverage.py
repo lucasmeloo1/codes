7a92663612413921f40422f949866152308a2681
lucasmeloo1 <lucasmelo130808@gmail.com>
2025-09-08 19:15:44 -0300

    Delete SchoolAverage.py

--- SchoolAverage.py
+++
@@ -1,7 +0,0 @@
-# Average of four student scores
-n = int(input('Enter a number:'))
-n1 = int(input('Enter another number:'))
-n2 = int(input('Enter one more number:'))
-n3 = int(input('Enter the last number:'))
-m = (n+n1+n2+n3)/4
-print('The average student score between {}, {}, {} and {} is {:.1f}'.format(n, n1, n2, n3, m))
