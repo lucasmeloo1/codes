f61ffec2dc93f29bde94775b4e8749ac96da3786
lucasmeloo1 <lucasmelo130808@gmail.com>
2025-09-08 22:41:18 -0300

    Add files via upload

---
+++ salary.py
@@ -0,0 +1,4 @@
+salary=int(input('Enter your salary: '))
+increase=salary*15/100
+newsalary=salary+increase
+print('Your salary is {}, with a 15% increase the new salary is {}'.format(salary, newsalary))
