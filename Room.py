49ac1fd0284365dbf553d9f252b191d121786315
lucasmeloo1 <lucasmelo130808@gmail.com>
2025-09-08 21:18:23 -0300

    Add files via upload

---
+++ Room.py
@@ -0,0 +1,5 @@
+w=int(input('Enter the width of the room in meters:'))
+h=int(input('Enter the height of the room in meters:'))
+a=w*h
+t=a/2
+print('The area of the room is {} square meters and the amount of paint needed to paint it is {} liters'.format(a, t))
