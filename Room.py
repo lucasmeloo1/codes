f026e2dfcf66f9ed687e187eddea7aae6bc68806
lucasmeloo1 <lucasmelo130808@gmail.com>
2025-09-08 21:19:53 -0300

    Delete Room.py

--- Room.py
+++
@@ -1,5 +0,0 @@
-w=int(input('Enter the width of the room in meters:'))
-h=int(input('Enter the height of the room in meters:'))
-a=w*h
-t=a/2
-print('The area of the room is {} square meters and the amount of paint needed to paint it is {} liters'.format(a, t))
