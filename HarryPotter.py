6219ad98da02e9d8e3ed5382fc4ca910dbb77fb5
lucasmeloo1 <lucasmelo130808@gmail.com>
2025-09-23 21:42:17 -0300

    Add files via upload

---
+++ HarryPotter.py
@@ -0,0 +1,9 @@
+# Write code below 💖
+
+
+print('Q1) Do you like Dawn or Dusk?\n 1)Dawn\n 2)Dusk')
+answer = (int(input('Enter your answer 1-4: ')))
+if answer == 1:
+  gryffindor =+ 26
+print(gryffindor)
+
