122be4deefeed925f410fd68fe886acc81b5ec0b
lucasmeloo1 <lucasmelo130808@gmail.com>
2025-10-02 17:50:26 -0300

    OddorEven.py

--- oddoreven.py
+++ oddoreven.py
@@ -1,11 +1,5 @@
-number=input('Enter a number between 1 and 20: ')
- 
-if number in ['1','3','5','7','9','11','13','15','17','19']:
- print(number, 'is odd')
-
-elif number in ['2','4','6','8','10','12','14','16','18','20']:
- print(number, 'is even')
-
+n=int(input('Enter a number:' ))
+if n%2==0:
+    print('The number {} is even'.format(n))
 else:
-    print('Invalid input.')
-    
+    print('The number {} is odd'.format(n))
