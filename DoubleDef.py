d183dcd7d5c710a432620c09286e421db3d8fe99
lucasmeloo1 <lucasmelo130808@gmail.com>
2025-10-05 23:38:01 -0300

    DoubleDef.py

--- DoubleDef.py
+++ DoubleDef.py
@@ -1,1 +1,10 @@
-print('1\n2\n3\n4\n5')
+import random
+
+while True:
+    n = random.randint(1,10)
+    def double(n):
+        return n * 2
+    print ('The number is {}, the result is {}.'.format(n, (double(n))))
+    again=input('Again?? (y/n): ').lower()
+    if again != "y":
+        break
