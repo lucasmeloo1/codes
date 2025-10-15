68f57b3a4b365bd57660da61657a2d4e3a4820fd
lucasmeloo1 <lucasmelo130808@gmail.com>
2025-10-15 20:56:14 -0300

    dog_years

---
+++ dog_years.py
@@ -0,0 +1,7 @@
+
+def dog_years(name, age):
+  return  (f"{name} is {age * 7} years old in human years.")
+
+print(dog_years('Landon', 3))
+print(dog_years('Red Bean', 6))
+print(dog_years('Cooper', 2))
